614db82f850fce0ffc698adcb08105fdf078b4cb
Mohammed Alostath <mostath91@gmail.com>
2025-08-20 13:49:22 +0300

    add dependency injection to main.dart / add multiBlocProvider to main.dart / add PostsPage as home to MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_posts/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart';
+import 'package:flutter_posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'features/posts/presentation/pages/posts_page.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -9,12 +16,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => di.sl<PostsBloc>()),
+        BlocProvider(create: (_) => di.sl<AddUpdateDeletePostBloc>()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: PostsPage(),
       ),
-      home: const Scaffold(body: Text('Flutter Demo Home Page')),
     );
   }
 }

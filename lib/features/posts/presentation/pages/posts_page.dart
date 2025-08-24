1dbe6e971a1b201642be252ea78032955a24d8a6
Mohammed Alostath <mostath91@gmail.com>
2025-08-24 13:13:39 +0300

    implement posts page

--- lib/features/posts/presentation/pages/posts_page.dart
+++ lib/features/posts/presentation/pages/posts_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_posts/core/widgets/loading_widget.dart';
+import 'package:flutter_posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:flutter_posts/features/posts/presentation/pages/post_add_update_page.dart';
+import 'package:flutter_posts/features/posts/presentation/widgets/posts_page/post_list_widget.dart';
+
+import '../widgets/posts_page/message_display_widget.dart';
 
 class PostsPage extends StatelessWidget {
   const PostsPage({super.key});
@@ -7,7 +14,51 @@ class PostsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Posts')),
-      body: Center(child: Text('Hello from Home Page')),
+      floatingActionButton: PostsFloatingActionButton(),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10), // ✅ correct
+      child: BlocBuilder<PostsBloc, PostsState>(
+        builder: (context, state) {
+          if (state is LoadingPostsState) {
+            return LoadingWidget();
+          } else if (state is PostsLoadedState) {
+            return PostListWidget(posts: state.posts);
+          } else if (state is ErrorPostsState) {
+            return MessageDisplayWidget(message: state.message);
+          }
+          return LoadingWidget();
+        },
+      ),
+    );
+  }
+}
+
+class PostsFloatingActionButton extends StatelessWidget {
+  const PostsFloatingActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => PostAddUpdatePage(isUpdatePost: false),
+          ),
+        );
+      },
+      shape: CircleBorder(),
+      child: Icon(Icons.add),
     );
   }
 }
